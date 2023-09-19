7bc5836cc61380b43feedee554f0252b4e7cc0e1
Abdussamad Mohammed <112030326+iitz3bsmd@users.noreply.github.com>
2023-09-19 03:53:34 +0300

    Update setup.py

--- simVSmodel/setup.py
+++ simVSmodel/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'comparetor = simVSmodel.starter_node:main'
+            'comparator = simVSmodel.starter_node:main'
         ],
     },
 )
